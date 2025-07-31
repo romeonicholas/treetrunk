a1b56cfd7f9ac3299681d64dda4779c7f25e81bf
romeonicholas <nicholas.romeo@code.berlin>
2025-07-31 16:11:51 +0200

    Update gmic command

--- edit-photo.ps1
+++ edit-photo.ps1
@@ -5,4 +5,4 @@ param(
 )
 
 # Command to run: 
-C:\Users\romeo\Downloads\gmic_3.5.5_cli_win64\gmic-3.5.5-cli-win64\gmic.exe input $originalFilepath -cl_comic 6,3,0,0,0.8225,7.353,39,1,0,20,3,0,0,0,0,0,0,0,50,50 -input $cutoutPath -blend alpha -output $editedFilepath
+gmic input $originalFilepath -cl_comic 6,3,0,0,0.8225,7.353,39,1,0,20,3,0,0,0,0,0,0,0,50,50 -input $cutoutPath -blend alpha -output $editedFilepath

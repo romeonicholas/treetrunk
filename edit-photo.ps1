cc1a627960b143edc42c70f74622abb81eda56b7
romeonicholas <nicholas.romeo@code.berlin>
2025-08-06 09:30:34 +0200

    Remove photo filter process from PS script

--- edit-photo.ps1
+++ edit-photo.ps1
@@ -4,5 +4,8 @@ param(
   [string]$cutoutPath
 )
 
-# Command to run: 
-gmic input $originalFilepath -crop 236,0,843,1079 -resize 1080,1920 -cl_comic 6,3,0,0,0.8225,7.353,39,1,0,20,3,0,0,0,0,0,0,0,50,50 -input $cutoutPath -blend alpha -output $editedFilepath
+# With comic effect:
+# gmic input $originalFilepath -crop 236,0,843,1079 -resize 1080,1920 -cl_comic 6,3,0,0,0.8225,7.353,39,1,0,20,3,0,0,0,0,0,0,0,50,50 -input $cutoutPath -blend alpha -output $editedFilepath
+
+# Without comic effect:
+gmic input $originalFilepath -crop 236,0,843,1079 -resize 1080,1920 -input $cutoutPath -blend alpha -output $editedFilepath

d959e644d7e035f94c68fbddb9878e5bb2e1958d
Nicholas <nicholas.romeo@code.berlin>
2025-08-16 14:58:00 +0200

    Add shadow to selfie preview cutout, properly align edited photo cutout

--- create-selfie-overlay.ps1
+++ create-selfie-overlay.ps1
@@ -4,4 +4,4 @@ param(
 
 magick $figureFolder\cutout.webp $figureFolder\cutout.png
 magick $figureFolder\selfie_frame.webp $figureFolder\selfie_frame.png
-gmic $figureFolder\cutout.png expand x,248 expand y,-128 shift 360,500 drop_shadow -20,0,20,0,0,0 $figureFolder\selfie_frame.png blend alpha output $figureFolder\overlay.png
+gmic $figureFolder\cutout.png expand x,248 expand y,-128 shift 260,400 drop_shadow -20,0,20,0,0,0 $figureFolder\selfie_frame.png blend alpha output $figureFolder\overlay.png
